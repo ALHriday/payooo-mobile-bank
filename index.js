5e4dc5caa8967717aa7663472009e90d69d37e94
hriday <alauddinhriday@gmail.com>
2024-09-20 08:05:54 +0600

    added login form

--- index.js
+++ index.js
@@ -1,72 +1,67 @@
-let addMoneyForm = document.getElementById('add-money-form');
-let cashOutForm = document.getElementById('cash-out-form');
-
+let addMoneyForm = document.getElementById("add-money-form");
+let cashOutForm = document.getElementById("cash-out-form");
 
 //...........Added Add Money..............
 
-document.getElementById('add-money-btn')
-    .addEventListener('click', function () {
-        addMoneyForm.classList.remove('hidden');
-        cashOutForm.classList.add('hidden');
+document.getElementById("add-money-btn").addEventListener("click", function () {
+  addMoneyForm.classList.remove("hidden");
+  cashOutForm.classList.add("hidden");
+});
 
-    });
+document
+  .getElementById("add-money")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
 
-document.getElementById('add-money')
-    .addEventListener('click', function (event) {
-        event.preventDefault();
+    const mainBalance = document.getElementById("main-balance").innerText;
+    const addMoneyAmount = document.getElementById("add-money-amount").value;
+    const mainBalanceNum = parseFloat(mainBalance);
+    const addMoneyAmountNum = parseFloat(addMoneyAmount);
 
-        const mainBalance = document.getElementById('main-balance').innerText;
-        const addMoneyAmount = document.getElementById('add-money-amount').value;
-        const mainBalanceNum = parseFloat(mainBalance);
-        const addMoneyAmountNum = parseFloat(addMoneyAmount);
+    const pinNumber = document.getElementById("pin-number").value;
 
-        const pinNumber = document.getElementById('pin-number').value;
+    if (pinNumber === "1234") {
+      const newBalance = mainBalanceNum + addMoneyAmountNum;
+      document.getElementById("main-balance").innerText = newBalance;
+    } else {
+      alert("Please Try Again later.");
+    }
 
-        if (pinNumber === '1234') {
-            const newBalance = mainBalanceNum + addMoneyAmountNum;
-            document.getElementById('main-balance').innerText = newBalance;
-        } else {
-            alert('Please Try Again later.')
-        }
-        
-        document.getElementById('add-money-amount').value = '';
-        document.getElementById('pin-number').value = '';
-        addMoneyForm.classList.add('hidden');
-    });
+    document.getElementById("add-money-amount").value = "";
+    document.getElementById("pin-number").value = "";
+    // addMoneyForm.classList.add('hidden');
+  });
 
 //...........Added Cash Out..............
 
+document.getElementById("cash-out-btn").addEventListener("click", function () {
+  cashOutForm.classList.remove("hidden");
+  addMoneyForm.classList.add("hidden");
+});
 
-document.getElementById('cash-out-btn')
-    .addEventListener('click', function () {
-        cashOutForm.classList.remove('hidden');
-        addMoneyForm.classList.add('hidden');
+document.getElementById("cash-out").addEventListener("click", function (event) {
+  event.preventDefault();
 
-    });
+  const mainBalance = document.getElementById("main-balance").innerText;
+  const cashOutAmount = document.getElementById("cash-out-amount").value;
+  const mainBalanceNum = parseFloat(mainBalance);
+  const cashOutAmountNum = parseFloat(cashOutAmount);
+
+  const pinNumber = document.getElementById("pin-number2").value;
+
+  if (pinNumber === "1234") {
+    const newBalance = mainBalanceNum - cashOutAmountNum;
+    document.getElementById("main-balance").innerText = newBalance;
+  } else {
+    alert("Please Try Again later.");
+  }
+
+  document.getElementById("cash-out-amount").value = "";
+  document.getElementById("pin-number2").value = "";
+  // cashOutForm.classList.add('hidden');
+});
 
 
-    document.getElementById('cash-out')
-    .addEventListener('click', function (event) {
-        event.preventDefault();
-
-        const mainBalance = document.getElementById('main-balance').innerText;
-        const cashOutAmount = document.getElementById('cash-out-amount').value;
-        const mainBalanceNum = parseFloat(mainBalance);
-        const cashOutAmountNum = parseFloat(cashOutAmount);
-
-        const pinNumber = document.getElementById('pin-number2').value;
-
-        if (pinNumber === '1234') {
-            const newBalance = mainBalanceNum - cashOutAmountNum;
-            document.getElementById('main-balance').innerText = newBalance;
-        } else {
-            alert('Please Try Again later.')
-        }
-        
-        document.getElementById('cash-out-amount').value = '';
-        document.getElementById('pin-number2').value = '';
-        cashOutForm.classList.add('hidden');
-    });
 
 
 
---
+++ login.js
@@ -0,0 +1,18 @@
+let loginBtn = document.getElementById('login-btn');
+let phoneNum = document.getElementById('phone-number');
+let pinNum = document.getElementById('pin-number');
+
+
+loginBtn.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const phoneNumV = phoneNum.value;
+    const pinNumV = pinNum.value;
+
+    if (phoneNumV === '01812345678' && pinNumV === '1234') {
+        window.location.href = './index.html';
+    } else {
+        alert('Your Phone Number or Pin is Wrong. Please Try Again Later.');
+    }
+
+});
